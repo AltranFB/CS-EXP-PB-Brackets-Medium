9f0288d19b8468436c17b802a5e42ca8c9411d43
Grokwik <frederic@bodin.site>
2017-01-04 12:20:24 +0100

    Replacing NUnit by the MS tests framework
    nunit.framework is dead, hail to
    Microsoft.VisualStudio.QualityTools.UnitTestFramework

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Project
 {
--- UnitTests/Tests.cs
+++ UnitTests/Tests.cs
@@ -1,53 +1,53 @@
-﻿using NUnit.Framework;
-using NFluent;
+﻿using NFluent;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Project
 {
-    [TestFixture]
+    [TestClass]
     public class Tests
     {
-        [Test]
+        [TestMethod]
         public void Example()
         {
             var answer = Solution.DoIt("{([]){}()}");
             Check.That(answer).IsTrue();
         }
 
-        [Test]
+        [TestMethod]
         public void Test1()
         {
             var answer = Solution.DoIt("{([{S}]]6K[()]}");
             Check.That(answer).IsFalse();
         }
 
-        [Test]
+        [TestMethod]
         public void Test2()
         {
             var answer = Solution.DoIt("{C{}[{[a]}RqhL]{y2}}");
             Check.That(answer).IsTrue();
         }
-        [Test]
+        [TestMethod]
         public void Test3()
         {
             var answer = Solution.DoIt("W12{}{}L{}");
             Check.That(answer).IsTrue();
         }
 
-        [Test]
+        [TestMethod]
         public void Test4()
         {
             var answer = Solution.DoIt("h{Pn{GT{h}(c))}");
             Check.That(answer).IsFalse();
         }
 
-        [Test]
+        [TestMethod]
         public void Test5()
         {
             var answer = Solution.DoIt("{[{iHTSc}]}p(R)m(){q({})");
             Check.That(answer).IsFalse();
         }
 
-        [Test]
+        [TestMethod]
         public void Test6()
         {
             var answer = Solution.DoIt("][");
